24a8e3e0368ae6c43d681324f9187458efe481ed
hahagiraffe <646268684@qq.com>
2019-03-16 16:16:15 +0800

    add head

---
+++ head.h
@@ -0,0 +1,15 @@
+#ifndef HEAD_H
+#define HEAD_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <vector>
+#include <string>
+#include <list>
+#include <map>
+#include <set>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+#endif
